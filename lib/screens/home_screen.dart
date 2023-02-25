a5d92534e05aadb1e071d3b0f7e2f203f8110f3d
martin-ngigi <martinwainaina001@gmail.com>
2023-02-25 09:19:31 +0300

    Load all chats success

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -3,9 +3,12 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:intl/intl.dart';
 
 import '../models/user_model.dart';
 import 'auth_screen.dart';
+import 'chat_screen.dart';
 import 'hs.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -36,7 +39,117 @@ class _HomeScreenState extends State<HomeScreen> {
               icon: const Icon(Icons.logout))
         ],
       ),
-      body: Center(child: Text(" Home Page")),
+      body: StreamBuilder(
+          stream: FirebaseFirestore.instance
+              .collection('user')
+              .doc(widget.user.uid)
+              .collection('messages')
+              .snapshots(),
+          builder: (context, AsyncSnapshot snapshot) {
+            if (snapshot.hasData) {
+              if (snapshot.data.docs.length < 1) {
+                return const Center(
+                  child: Text("No Chats Available !"),
+                );
+              }
+              return ListView.builder(
+                  itemCount: snapshot.data.docs.length,
+                  itemBuilder: (context, index) {
+                    var friendId = snapshot.data.docs[index].id;
+                    var lastMsg = snapshot.data.docs[index]['last_msg'];
+                    // var date = snapshot.data.docs[index]['date'];
+                    var a = DateTime.parse(
+                        snapshot.data.docs[index]['date'].toDate().toString());
+                    var time = DateFormat(' hh:mm a').format(a);
+
+                    return FutureBuilder(
+                      future: FirebaseFirestore.instance
+                          .collection('user')
+                          .doc(friendId)
+                          .get(),
+                      builder: (context, AsyncSnapshot asyncSnapshot) {
+                        if (asyncSnapshot.hasData) {
+                          var friend = asyncSnapshot.data;
+                          return ListTile(
+                            leading: ClipRRect(
+                              borderRadius: BorderRadius.circular(60),
+                              child: Container(
+                                margin: const EdgeInsets.only(),
+                                width: 58.0,
+                                height: 65.0,
+                                decoration: const BoxDecoration(
+                                  shape: BoxShape.circle,
+                                ),
+                                child: ClipOval(
+                                  child: Image.network(
+                                    friend['image'],
+                                    fit: BoxFit.cover,
+                                  ),
+                                ),
+                              ),
+                            ),
+                            title: Text(
+                              friend['name'],
+                              style: const TextStyle(
+                                fontSize: 17,
+                                fontWeight: FontWeight.w500,
+                              ),
+                            ),
+                            // ignore: avoid_unnecessary_containers
+                            subtitle: Container(
+                              child: Row(
+                                children: <Widget>[
+                                  Container(
+                                    width: 200,
+                                    height: 40,
+                                    child: Text(
+                                      "$lastMsg",
+                                      style: const TextStyle(color: Colors.grey),
+                                      overflow: TextOverflow.ellipsis,
+                                      maxLines: 1,
+                                    ),
+                                  ),
+                                  Container(
+                                    margin: const EdgeInsets.only(
+                                      left: 40.0,
+                                    ),
+                                    child: Text(
+                                      time,
+                                      style: const TextStyle(
+                                          color: Colors.grey, fontSize: 10),
+                                      overflow: TextOverflow.ellipsis,
+                                      maxLines: 1,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                              // child: Text(
+                              //   "$lastMsg",
+                              //   style: const TextStyle(color: Colors.grey),
+                              //   overflow: TextOverflow.ellipsis,
+                              // ),
+                            ),
+                            onTap: () {
+                              Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) => ChatScreen(
+                                          currentUser: widget.user,
+                                          friendId: friend['uid'],
+                                          friendName: friend['name'],
+                                          friendImage: friend['image'])));
+                            },
+                          );
+                        }
+                        return const LinearProgressIndicator();
+                      },
+                    );
+                  });
+            }
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }),
       floatingActionButton: FloatingActionButton(
         backgroundColor: Colors.deepOrange,
         child: const Icon(Icons.message, ),
