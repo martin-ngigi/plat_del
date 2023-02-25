707e0162abc6c11c0e68eb84ab3b441a06a03203
martin-ngigi <martinwainaina001@gmail.com>
2023-02-25 06:38:24 +0300

    Chatting success

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
 import '../models/user_model.dart';
+import '../widgets/message_textfield.dart';
+import '../widgets/single_message.dart';
 
 class ChatScreen extends StatelessWidget {
   final UserModel currentUser;
@@ -52,8 +54,56 @@ class ChatScreen extends StatelessWidget {
           ],
         ),
       ),
-      body: Center(
-        child: Text("Chat Screen"),
+      body: Column(
+        children: [
+          Expanded(
+              child: Container(
+                padding: const EdgeInsets.all(10),
+                decoration: const BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(25),
+                        topRight: Radius.circular(25))),
+                child: StreamBuilder(
+                    stream: FirebaseFirestore.instance
+                        .collection("user")
+                        .doc(currentUser.uid)
+                        .collection('messages')
+                        .doc(friendId)
+                        .collection('chats')
+                        .orderBy("date", descending: true)
+                        .snapshots(),
+                    builder: (context, AsyncSnapshot snapshot) {
+                      if (snapshot.hasData) {
+                        if (snapshot.data.docs.length < 1) {
+                          return const Center(
+                            child: Text("Say Hi"),
+                          );
+                        }
+
+                        return ListView.builder(
+                            itemCount: snapshot.data.docs.length,
+                            reverse: true,
+                            physics: const BouncingScrollPhysics(),
+                            itemBuilder: (context, index) {
+                              var a = DateTime.parse(snapshot
+                                  .data.docs[index]['date']
+                                  .toDate()
+                                  .toString());
+                              var time = DateFormat(' hh:mm a').format(a);
+                              bool isMe = snapshot.data.docs[index]['senderId'] ==
+                                  currentUser.uid;
+                              return SingleMessage(
+                                  message: snapshot.data.docs[index]['message'],
+                                  isMe: isMe,
+                                  time: time);
+                            });
+                      }
+                      return const Center(child: CircularProgressIndicator());
+                    }),
+              )),
+          MessageTextField(currentUser.uid, friendId),
+        ],
       ),
     );
   }
---
+++ lib/widgets/single_message.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+
+class SingleMessage extends StatelessWidget {
+  final String message;
+  final bool isMe;
+  final String time;
+
+  const SingleMessage(
+      {super.key,
+      required this.message,
+      required this.isMe,
+      required this.time});
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: isMe ? MainAxisAlignment.end : MainAxisAlignment.start,
+      children: [
+        Container(
+          padding: const EdgeInsets.all(16),
+          margin: const EdgeInsets.all(16),
+          constraints: const BoxConstraints(maxWidth: 200),
+          decoration: BoxDecoration(
+              color: isMe ? Colors.grey[200] : Colors.blue[100],
+              borderRadius: const BorderRadius.all(Radius.circular(12))),
+          // child: Text(
+          //   message,
+          //   style: const TextStyle(
+          //     color: Colors.white,
+          //   ),
+          // )
+          child: Column(
+            children: <Widget>[
+              Text(
+                message,
+                style: const TextStyle(
+                  fontSize: 15,
+                  color: Colors.black,
+                  fontWeight: FontWeight.bold
+                ),
+              ),
+              Container(
+                margin: const EdgeInsets.only(
+                  left: 0.0,
+                ),
+                child: Text(
+                  time,
+                  style: const TextStyle(
+                    fontSize: 10,
+                    color: Colors.black,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
